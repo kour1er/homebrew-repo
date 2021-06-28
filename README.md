a64786da074c46a12d3477096f56b7775f3abf31
kour1er <34779600+kour1er@users.noreply.github.com>
2021-06-28 15:16:21 +0000

    Updated to latest dovecot and pigeonhole

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # homebrew-repo
 
-Simple collection of some alternative formulas for Homebrew. 
+Simple collection of some alternative formulas for Homebrew
+
+Dovecot-dev
+    This includes the xaps plugin and support of libstemmer (below)
+
+Libstemmer-dev
+
+RSpamD-dev
--- dovecot-dev.rb
+++ dovecot-dev.rb
@@ -1,8 +1,8 @@
 class DovecotDev < Formula
   desc "IMAP/POP3 server"
   homepage "http://dovecot.org/"
-  url "https://dovecot.org/releases/2.3/dovecot-2.3.14.tar.gz"
-  version "2.3.14"
+  url "https://dovecot.org/releases/2.3/dovecot-2.3.15.tar.gz"
+  version "2.3.15"
   sha256 "c8b3d7f3af1e558a3ff0f970309d4013a4d3ce136f8c02a53a3b05f345b9a34a"
   license all_of: ["BSD-3-Clause", "LGPL-2.1-or-later", "MIT", "Unicode-DFS-2016", :public_domain]
 
@@ -15,7 +15,7 @@ class DovecotDev < Formula
   uses_from_macos "sqlite"
 
   resource "pigeonhole" do
-    url "https://pigeonhole.dovecot.org/releases/2.3/dovecot-2.3-pigeonhole-0.5.14.tar.gz"
+    url "https://pigeonhole.dovecot.org/releases/2.3/dovecot-2.3-pigeonhole-0.5.15.tar.gz"
     sha256 "68ca0f78a3caa6b090a469f45c395c44cf16da8fcb3345755b1ca436c9ffb2d2"
   end
 
