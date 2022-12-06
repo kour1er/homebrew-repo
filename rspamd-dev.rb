9fc9ad09c5531504eff97b30246e1b23d2c62061
kour1er <34779600+kour1er@users.noreply.github.com>
2022-12-06 16:22:06 +0000

    Updated to latest 3.4 source

--- rspamd-dev.rb
+++ rspamd-dev.rb
@@ -1,9 +1,9 @@
 class RspamdDev < Formula
   desc "Rspamd filtering system is created as a replacement of popular spamassassin spamd and is designed to be fast, modular and easily extendable system."
   homepage "https://github.com/rspamd/rspamd/"
-  url "https://github.com/rspamd/rspamd/archive/3.3.tar.gz"
-  version "3.3"
-  sha256 "4d6e90facd0f7705d11d5073dba22b5d6686097798178b3b3e965a989ba7be0f"
+  url "https://github.com/rspamd/rspamd/archive/refs/tags/3.4.tar.gz"
+  version "3.4"
+  sha256 "f8d3e2b9a1a6ed6521c60fe505e97086624407f67366f0ce882eee433a53c355"
 
   depends_on "cmake"
   depends_on "glib-openssl"
